ef583bb620cbaf21c42d6e4d6b3b0e5e51b9d179
Camellia <56206613+sialiss@users.noreply.github.com>
2023-07-08 00:47:15 +0300

    Enable plugin

--- Scenes/Enemies/Fish/EnemyFish.gd
+++ Scenes/Enemies/Fish/EnemyFish.gd
@@ -1,11 +1,32 @@
 extends Enemy
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	print("int_range 1~100 : ", RngUtils.int_range(1, 100))
+	
+# func set_location(location: Node2D):
+# 	# Set the mob's direction perpendicular to the path direction.
+# 	var direction = location.global_roгtation + PI / 2
 
+# 	# Set the mob's position to a random location.
+# 	global_position = location.global_position
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+# 	# Add some randomness to the direction.
+# 	direction += randf_range(-PI / 4, PI / 4)
+# 	rotation = direction
+
+# 	# Choose the velocity
+# 	var velocity = Vector2(randf_range(150.0, 250.0), 0.0)
+# 	linear_velocity = velocity.rotated(direction)
+
+# func _process(delta):
+# 	$HatOrigin.global_rotation = 0
+# 	$HatOrigin/EnemyHatDisplay.display3d.set_hat_rotation(global_rotation)
+
+# func _on_VisibilityNotifier2D_screen_exited():
+# 	queue_free()
+
+# func die():
+# 	$CollisionShape2D.set_deferred("disabled", true)
+# 	var tween = create_tween()
+# 	tween.tween_property(self, "modulate", Color.TRANSPARENT, 0.5)
+# 	tween.tween_callback(Callable(self, "queue_free"))
