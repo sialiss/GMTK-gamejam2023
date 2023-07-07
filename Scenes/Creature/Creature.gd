9b91fce2ddfd35aa392b90474c0febcefd1ec3b0
och-och <och.hco.och.hco@gmail.com>
2023-07-07 23:46:45 +0300

    rotating creature

--- Scenes/Creature/Creature.gd
+++ Scenes/Creature/Creature.gd
@@ -1,5 +1,17 @@
 extends RigidBody2D
 
 
+var force = 200
+var torque = 1000
+
+
 func _physics_process(delta):
-	constant_force = Input.get_vector("move_left", "move_right", "move_up", "move_down") * 100
+	var input = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+
+	# Move
+	constant_force = input * force
+	
+
+	var angle = Vector2.UP.rotated(global_rotation).angle_to(input)
+	constant_torque = angle * torque
+
