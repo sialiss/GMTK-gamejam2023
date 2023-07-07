63ccc6deac241ee3142e8e12901c86ab158ce967
Camellia <56206613+sialiss@users.noreply.github.com>
2023-07-08 02:15:11 +0300

    Add fish spawn

--- Scenes/Enemies/Fish/EnemyFish.gd
+++ Scenes/Enemies/Fish/EnemyFish.gd
@@ -1,32 +1,31 @@
 extends Enemy
 
 func _ready():
-	print("int_range 1~100 : ", RngUtils.int_range(1, 100))
-	
-# func set_location(location: Node2D):
-# 	# Set the mob's direction perpendicular to the path direction.
-# 	var direction = location.global_roгtation + PI / 2
+	pass
 
-# 	# Set the mob's position to a random location.
-# 	global_position = location.global_position
+func set_location(location: Node2D):
+	# Set the mob's direction perpendicular to the path direction.
+	var direction = location.rotation + PI / 2
 
-# 	# Add some randomness to the direction.
-# 	direction += randf_range(-PI / 4, PI / 4)
-# 	rotation = direction
+	# Set the mob's position to a random location.
+	global_position = location.global_position
 
-# 	# Choose the velocity
-# 	var velocity = Vector2(randf_range(150.0, 250.0), 0.0)
-# 	linear_velocity = velocity.rotated(direction)
+	# Add some randomness to the direction.
+	direction += randf_range(-PI / 4, PI / 4)
+	rotation = direction
 
-# func _process(delta):
-# 	$HatOrigin.global_rotation = 0
-# 	$HatOrigin/EnemyHatDisplay.display3d.set_hat_rotation(global_rotation)
+	# Choose the velocity
+	var velocity = Vector2(randf_range(150.0, 250.0), 0.0)
+	linear_velocity = velocity.rotated(direction)
 
-# func _on_VisibilityNotifier2D_screen_exited():
-# 	queue_free()
+func _process(delta):
+	pass
 
-# func die():
-# 	$CollisionShape2D.set_deferred("disabled", true)
-# 	var tween = create_tween()
-# 	tween.tween_property(self, "modulate", Color.TRANSPARENT, 0.5)
-# 	tween.tween_callback(Callable(self, "queue_free"))
+func _on_VisibilityNotifier2D_screen_exited():
+	queue_free()
+
+func die():
+	$CollisionShape2D.set_deferred("disabled", true)
+	var tween = create_tween()
+	tween.tween_property(self, "modulate", Color.TRANSPARENT, 0.5)
+	tween.tween_callback(Callable(self, "queue_free"))
