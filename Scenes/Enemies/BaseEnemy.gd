84142ef8afcd3e4e1bb3c2446666a67847ad9d48
Camellia <56206613+sialiss@users.noreply.github.com>
2023-07-09 17:49:16 +0300

    Add death to bases and submarines

--- Scenes/Enemies/BaseEnemy.gd
+++ Scenes/Enemies/BaseEnemy.gd
@@ -11,4 +11,7 @@ func harm(taken_damage: float):
 		die()
 
 func die():
-	queue_free()
+	$CollisionShape2D.set_deferred("disabled", true)
+	var tween = create_tween()
+	tween.tween_property(self, "modulate", Color.TRANSPARENT, 0.5)
+	tween.tween_callback(Callable(self, "queue_free"))
