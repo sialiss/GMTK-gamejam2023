ef7f9e932c2837777dcfa81850ce378120d98048
och-och <och.hco.och.hco@gmail.com>
2023-07-09 15:18:32 +0300

    fishies

--- Scenes/Enemies/Fishes/EnemyFish.gd
+++ Scenes/Enemies/Fishes/EnemyFish.gd
@@ -1,16 +1,18 @@
 extends Fish
 
+
+var target_position = Vector2.ZERO
+
+
 func _physics_process(delta):
-	var target_position
-	var input
 	if PositionNotifier.has_overlapping_bodies():
 		# игрок в области видимости рыбки
-		target_position = Sea.sea.get_node_or_null("Creature")
-		input = global_position.direction_to(target_position.global_position)
+		target_position = Sea.sea.get_node("Creature").global_position
 	else:
 		# игрока рядом нет, рыбка плывёт домой
-		target_position = targetHome
-		input = global_position.direction_to(targetHome.global_position)
+		target_position = targetHome.global_position
+	var input = global_position.direction_to(target_position)
 
 	# Move
 	linear_velocity = linear_velocity.move_toward(input * speed, acceleration * delta)
+	
--- Scenes/Enemies/Fishes/NeutralFish.gd
+++ Scenes/Enemies/Fishes/NeutralFish.gd
@@ -13,4 +13,17 @@ func _physics_process(delta):
 		input = global_position.direction_to(targetHome.global_position)
 
 	# Move
-	linear_velocity = linear_velocity.move_toward(input * speed, acceleration * delta)
+	# linear_velocity = linear_velocity.move_toward(input * speed, acceleration * delta)
+	constant_force = input * speed
+
+	# Rotate
+	# var old_rotation = global_rotation
+	# look_at(global_position + input)
+	# var target_rotation = global_rotation
+	# rotation = old_rotation
+
+	# var old_rotation = Vector2.from_angle(global_rotation)
+	# var target_rotation = constant_force.normalized()
+	# constant_torque = old_rotation.angle_to(target_rotation) * 1000
+
+	$Sprite2D.look_at(global_position + linear_velocity)
